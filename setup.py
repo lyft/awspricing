1c5a055e79b8491a946d6d6d1d021ddd4ddac95f
Anthony Sottile <asottile@umich.edu>
2018-06-18 09:34:26 -0700

    Only depend on typing in python 2 (#17)

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import os
 import re
 
@@ -8,13 +7,6 @@ from setuptools import setup, find_packages
 
 ROOT = os.path.dirname(__file__)
 VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
-
-
-install_requires = [
-    'requests>=2.5.0,<3.0.0',    # License: Apache2
-    'six>=1.0.0,<2.0.0',         # License: MIT
-    'typing>=3.5.0.0,<3.6.0.0',  # License: PSF
-]
 
 
 def get_version():
@@ -28,12 +20,13 @@ setup(
     description='An SDK for AWS Pricing',
     long_description=open('README.rst').read(),
     author='Garrett Heel',
-    url='https://github.com/garrettheel/aws-pricing',
-    scripts=[],
+    url='https://github.com/lyft/awspricing',
     packages=find_packages(exclude=['tests*']),
-    package_data={},
-    include_package_data=True,
-    install_requires=install_requires,
+    install_requires=[
+        'requests>=2.5.0,<3.0.0',    # License: Apache2
+        'six>=1.0.0,<2.0.0',         # License: MIT
+    ],
+    extras_require={':python_version=="2.7"': ['typing>=3.5.0.0,<3.6.0.0']},
     license="apache2",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -41,10 +34,9 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
