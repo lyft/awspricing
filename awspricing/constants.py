23493d2c397da9dcffcf3b598df5b5d32f303121
DingGGu <dingggu@gmail.com>
2019-01-19 07:48:10 +0900

    Added more aws region (#34)

--- awspricing/constants.py
+++ awspricing/constants.py
@@ -35,15 +35,20 @@ REGION_SHORTS = {
     'us-west-1': 'US West (N. California)',
     'us-west-2': 'US West (Oregon)',
     'ca-central-1': 'Canada (Central)',
+    'eu-north-1': 'EU (Stockholm)',
     'eu-west-1': 'EU (Ireland)',
     'eu-central-1': 'EU (Frankfurt)',
     'eu-west-2': 'EU (London)',
+    'eu-west-3': 'EU (Paris)',
     'ap-northeast-1': 'Asia Pacific (Tokyo)',
     'ap-northeast-2': 'Asia Pacific (Seoul)',
+    'ap-northeast-3': 'Asia Pacific (Osaka-Local)',
     'ap-southeast-1': 'Asia Pacific (Singapore)',
     'ap-southeast-2': 'Asia Pacific (Sydney)',
     'ap-south-1': 'Asia Pacific (Mumbai)',
-    'sa-east-1': 'South America (Sao Paulo)',  # intentionally no unicode
+    'sa-east-1': 'South America (Sao Paulo)',  # intentionally no unicode,
+    'us-gov-west-1': 'AWS GovCloud (US)',
+    'us-gov-east-1': 'AWS GovCloud (US-East)'
 }
 
 
